3a9929ca65e774d4c2d09c6b1c53103d65f84d01
Daniel Ma <drailskid@yahoo.com>
2016-02-20 23:23:25 -0800

    Initial Commit

---
+++ robomodoro/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  robomodoro
+//
+//  Created by Daniel Ma on 2/20/16.
+//  Copyright © 2016 Daniel Ma. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
